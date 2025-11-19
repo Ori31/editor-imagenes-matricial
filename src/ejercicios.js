adb79cc612d92f4819fb3c5d11aee8330a1d29f0
Ori <shpersonal000@gmail.com>
2025-11-18 19:18:52 -0600

    Ejercicio 2.3

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -267,16 +267,22 @@ function invertirColores(matriz) {
  * const grises = convertirEscalaGrises(matriz);
  */
 function convertirEscalaGrises(matriz) {
-  // TODO: Implementar conversión a escala de grises
-  
-  // Para cada pixel:
-  // 1. Calcular el valor de gris
-  // const gris = 0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b;
-  // 
-  // 2. Asignar ese valor a los tres canales
-  // pixelNuevo = {r: gris, g: gris, b: gris, a: pixel.a}
-  
-  return []; // REEMPLAZAR
+  const resultado = copiarMatriz(matriz);
+
+  for (let i = 0; i < resultado.length; i++) {
+    for (let j = 0; j < resultado[i].length; j++) {
+      const pixel = matriz[i][j];
+      const gris = 0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b;
+      resultado[i][j] = {
+        r: gris,
+        g: gris,
+        b: gris,
+        a: pixel.a
+      };
+    }
+  }
+
+  return resultado;
 }
 
 // ============================================
